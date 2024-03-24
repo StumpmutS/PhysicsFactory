bf396f7e5a00e23056ad1c26284a83c28ad99bda
drewm <drew.m.moulton@gmail.com>
2024-03-23 19:09:29 -0700

    Fix snapping issue on odd cells

--- Assets/General/Snapping/SnapTarget.cs
+++ Assets/General/Snapping/SnapTarget.cs
@@ -21,15 +21,19 @@ public class SnapTarget : MonoBehaviour
 
     private void Snap(Transform snappable, Vector3 snapPoint)
     {
-        var extents = mainTransform.localScale / 2;
+        var extents = (mainTransform.parent.rotation * mainTransform.localScale / 2).Abs();
         var snapDirection = snapPoint - mainTransform.position;
         var weightedDir = Vector3.Scale(snapDirection, extents.Invert());
         var face = weightedDir.IsolateAxis().normalized;
 
         var snapOffset = Vector3.Scale(face, extents) + face * snappable.localScale.z / 2;
-        var cellOffset = Vector3.Scale(Grid3D.ConvertToCellPosition(snapDirection), Vector3.one - face.Abs());
+        // Only includes face axis
+        var snapPos = Vector3.Scale(mainTransform.position + snapOffset, face.Abs());
+        // Only includes non-face axes
+        var cellPos = Vector3.Scale(Grid3D.ConvertToCellPosition(snapPoint), Vector3.one - face.Abs());
+        
         snappable.rotation = Quaternion.LookRotation(face);
-        snappable.position = mainTransform.position + snapOffset + cellOffset;
+        snappable.position = snapPos + cellPos;
 
         OnSnap.Invoke();
     }
